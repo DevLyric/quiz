e0664d77fe3d9c8a8d22649833ae93e1ffa2c952
Douglas Souza <lyricsan@hotmail.com>
2024-02-24 00:02:11 -0300

    feat: add app layout

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,9 +1,17 @@
 import { createBrowserRouter } from 'react-router-dom'
+
+import { AppLayout } from './pages/_layouts/app'
 import { Home } from './pages/home'
 
 export const routes = createBrowserRouter([
   {
     path: '/',
-    element: <Home />
+    element: <AppLayout />,
+    children: [
+      {
+        path: '/',
+        element: <Home />
+      }
+    ]
   }
 ])
