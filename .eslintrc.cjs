b9710eca7c36ad77a0873c64412a4330f06c059a
Douglas Souza <lyricsan@hotmail.com>
2024-02-23 23:35:32 -0300

    first commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    "@rocketseat/eslint-config/react"
+  ],
+  plugins: ["simple-import-sort"],
+  rules: {
+    "simple-import-sort/imports": "error"
+  }
+}
